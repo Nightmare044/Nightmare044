1483fb98b22c08f52848790dd6acf1d03b01dd27
Oleg <olegvinarchik2@gmail.com>
2022-05-15 18:49:46 +0300

    add url + transfer files to HTML

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,5 +1,21 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'weather/index.html')
+    appid = '29612e15df8d0d08f332cafc69daf449'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    city = 'London'
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"]
+    }
+
+    #transfer files to HTML
+    context = {'info': city_info}
+
+    return render(request, 'weather/index.html', context)
